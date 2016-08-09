2a92cfd9d56220cd6c7613180990a2bf26eb43e8
unknown <>
2016-08-09 17:53:24 +0900

    first commit

---
+++ src/main/java/devfun/bookstore/common/config/AppConfig.java
@@ -0,0 +1,9 @@
+package devfun.bookstore.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/*데이터베이스 접속, 트랜잭션 관리, DAO와 Service정의함.*/
+
+@Configuration
+public class AppConfig{
+}
---
+++ src/main/java/devfun/rest/config/RestAppConfig.java
@@ -0,0 +1,13 @@
+package devfun.rest.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/*
+REST 웹 애플리케이션 설정 클래스
+web.xml에 선언한 DispatcherServlet에 의해 처리되려면
+패키지명을 devfun.bookstore.rest.config로 해야한다. 
+*/
+@Configuration
+public class RestAppConfig{
+	
+}
